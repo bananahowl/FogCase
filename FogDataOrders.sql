062f8e6989409f85d6940d59da9427597af92c36
A O <ahmedothman8@hotmail.com>
2019-04-25 13:38:00 +0200

    values to H,L,W and for degrees

--- FogDataOrders.sql
+++ FogDataOrders.sql
@@ -26,9 +26,9 @@ heightID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 heightValue int(11) NOT NULL
 );
 
-CREATE TABLE if not exists angles(
-anglesID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-anglesValue int(11) NOT NULL
+CREATE TABLE if not exists degrees(
+degreeID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+degreeValue int(11) NOT NULL
 );
 
 CREATE TABLE if not exists roofmaterial(
@@ -45,7 +45,8 @@ lengthFK int(11),
 height int(11),
 degree int(11),
 FOREIGN KEY (lengthFK) REFERENCES length(lengthID),
-FOREIGN KEY (widthFK) REFERENCES width(widthID)
+FOREIGN KEY (widthFK) REFERENCES width(widthID),
+FOREIGN KEY (degree) REFERENCES degrees(degreeID)
 
 );
 #rename table area to arearoof; 
@@ -66,7 +67,7 @@ materialFK int(11),
 FOREIGN KEY (materialFK) REFERENCES materials(materialID),
 FOREIGN KEY (lengthFK) REFERENCES length(lengthID),
 FOREIGN KEY (widthFK) REFERENCES width(widthID),
-FOREIGN KEY (heightFK) REFERENCES length(heightID)
+FOREIGN KEY (heightFK) REFERENCES height(heightID)
 );
 
 
--- FogDataValues.sql
+++ FogDataValues.sql
@@ -15,18 +15,71 @@ insert into roofmaterial (roofmaterialname, roofmaterialprice) values ("plast", 
 
 select * from roofmaterial;
 
+
+insert into width(widthValue) values(240);
+insert into width(widthValue) values(270);
+insert into width(widthValue) values(300);
+insert into width(widthValue) values(330);
+insert into width(widthValue) values(360);
+insert into width(widthValue) values(390);
+insert into width(widthValue) values(420);
+insert into width(widthValue) values(450);
+insert into width(widthValue) values(480);
+insert into width(widthValue) values(510);
+insert into width(widthValue) values(540);
+insert into width(widthValue) values(570);
+insert into width(widthValue) values(600);
+insert into width(widthValue) values(630);
+insert into width(widthValue) values(660);
+insert into width(widthValue) values(690);
+insert into width(widthValue) values(720);
+insert into width(widthValue) values(750);
+select * from width;
+
+
+select * from length;
+#insert into length(lengthValue) values(240);
+insert into length(lengthValue) values(270);
+insert into length(lengthValue) values(300);
+insert into length(lengthValue) values(330);
+insert into length(lengthValue) values(360);
+insert into length(lengthValue) values(390);
+insert into length(lengthValue) values(420);
+insert into length(lengthValue) values(450);
+insert into length(lengthValue) values(480);
+insert into length(lengthValue) values(510);
+insert into length(lengthValue) values(540);
+insert into length(lengthValue) values(570);
+insert into length(lengthValue) values(600);
+insert into length(lengthValue) values(630);
+insert into length(lengthValue) values(660);
+insert into length(lengthValue) values(690);
+insert into length(lengthValue) values(720);
+insert into length(lengthValue) values(750);
+insert into length(lengthValue) values(780);
+
+insert into degrees (degreeValue) values(15);
+insert into degrees (degreeValue) values(20);
+insert into degrees (degreeValue) values(30);
+insert into degrees (degreeValue) values(35);
+insert into degrees (degreeValue) values(40);
+insert into degrees (degreeValue) values(45);
+ 
+ select * from degrees;
+
+/*
 select * from arearoof;
 
 insert into roof (roofmaterialFK, areaFK) values ();
 select * from arearoof;
-insert into arearoof (width,length,heigth,degree) values (20,20,15,40);
+insert into arearoof (width,length,height,degree) values (2,4,1,40);
 
 select * from carport;
-insert into carport (width, length, heigth, materialFK) values (20,20,40,2);
+insert into carport (width, length, height, materialFK) values (2,3,1,2);
 
 
 select * from shed;
-insert into shed (width,length, heigth, materialFK) values(15,15,35,1);
+insert into shed (width,length, height, materialFK) values(1,1,1,1);
 #REname table arearoof to area;
 
 select * from roof;
@@ -34,3 +87,15 @@ insert into roof (roofmaterialFK, areaFK) values (1,1);
 
 select * from orders;
 insert into orders (carportFK, shedFK, roofFK) values (1,1,1);
+
+select * from width;
+
+insert into width(widthValue) values(240);
+insert into width(widthValue) values(240);
+
+select * from height;
+insert into height (heightValue) values(200);
+
+select * from degrees;
+*/
+
