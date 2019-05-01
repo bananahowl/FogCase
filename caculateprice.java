d7316c0c6678169e302df0d32602d07f07432508
A O <ahmedothman8@hotmail.com>
2019-05-01 12:56:15 +0200

    mades

--- caculateprice.java
+++ caculateprice.java
@@ -2,50 +2,59 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+*
+* Used to calcuate the price of the wood for the carport.
  */
 package Logic;
 
-
 import DataLayer.MaterialList;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
  *
- * @author frizz
+ * @author ahmed
  */
-public class CalcPartListtest {
-    public static void main(String[] args) throws CarportException, SQLException {
-        //ArrayList answer = CalcPartList.flatRoof();
-        
-        //System.out.println(answer);
-        
-        System.out.println("_____________________");
-        
-        //int answer1 = CalcPartList.calcShedMats();
-        //System.out.println(answer1);
-        
-        System.out.println("______________________");
-           
+public class CalcPrice {
+
+    public int woodPrice(ArrayList <MaterialList> list ){
+
+        int lenghtVal = 10;
+        int price = 5;
+        int total = 0;
+
+
+         for(int i = 0; i< list.size(); i++){
+            total +=(( list.get(i).getAmount() *  list.get(i).getLength()) /lenghtVal ) * price ;
+               System.out.println( list.get(i));
+
+               System.out.println("Lenght of material: "+ list.get(i).getLength());
+               System.out.println("Amount of material: "+list.get(i).getAmount());
+               System.out.println("-------------");
+               System.out.println( "current total: " + total);
+
+          }
+        return total ;
+        }
+
+/* sample for test
+
+/*
+
+System.out.println("______________________");
+
         ArrayList<MaterialList> shedtest = CalcPartList.calcShedMats(270);
         ArrayList<MaterialList> flattest = CalcPartList.flatRoof(600 , 350);
         ArrayList<MaterialList> posttest = CalcPartList.calculatePortPost(350, 780);
-          
+
         ArrayList<MaterialList> superlist = CalcPartList.totalMaterial(shedtest, flattest, posttest);
         System.out.println(superlist);
         //tostring
           for(int i = 0; i< superlist.size(); i++){
                System.out.println( superlist.get(i));
-           
+
           }
           CalcPrice test = new CalcPrice();
+
           
-          
-      
-        System.out.println("______________________ \n");
-        
-        //System.out.println(test.woodPrice(superlist));
-        test.woodPrice(superlist);
-    }
-    
+*/
 }
