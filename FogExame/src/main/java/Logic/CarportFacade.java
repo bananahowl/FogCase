54722aa4e6375d0da8d95b07f269ef2818773bce
fred8728 <fskn@hotmail.com>
2019-05-08 12:01:47 +0200

    dk

--- FogExame/src/main/java/Logic/CarportFacade.java
+++ FogExame/src/main/java/Logic/CarportFacade.java
@@ -135,12 +135,6 @@ public class CarportFacade {
         return null;
     }
 
-    public static User createUser(String email, String password) throws CarportException {
-        User user = new User(email, password);
-        CarportMapper.createUser(user);
-        return user;
-    }
-
     public static CarportWithShed add(Carport carport, Shed shed) {
         CarportWithShed cws = new CarportWithShed(carport, shed);
         return cws;
---
+++ FogExame/target/FogExame-1.0-SNAPSHOT/Register.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Register
+    Created on : 08-05-2019, 12:00:22
+    Author     : fskn
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
