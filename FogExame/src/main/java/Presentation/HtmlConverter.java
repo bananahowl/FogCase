6ef42378a77d1bd6645c20caa441b01b7f9b12a6
Emil-Skovbo <emilskovbo18@gmail.com>
2019-05-08 12:48:54 +0200

    more commands

--- FogExame/src/main/java/Presentation/HtmlConverter.java
+++ FogExame/src/main/java/Presentation/HtmlConverter.java
@@ -5,7 +5,6 @@
  */
 package Presentation;
 
-
 import DataLayer.Carport;
 import java.util.ArrayList;
 
@@ -16,11 +15,15 @@ import java.util.ArrayList;
 public class HtmlConverter {
 
     public static String carportFlatRooftoHtml(Carport carport) {
-        String cartTable = "<table id=\"carport\">"
-                + "<tr><th>Carport</th><th>Length = </th><th>Width = </th><th>Height = 220</th></tr>"
-                + "<tr><th>Size</th><th>" + carport.getLength() + "</th><th>" + carport.getWidth() + "</th></tr>"
-                + "</table>";
+        String cartTable = "<form action=\"FrontController\" method=\"GET\">"
+                + "<table id=\"carport\">"
+                + "<tr><th>Carport  </th><th>Length   </th><th>Width    </th><th>Height    </th><th>Angle   </th><th>Price      </th></tr>"
+                + "<tr><th>Size</th><th>" + carport.getLength() + "</th><th>" + carport.getWidth() + "</th><th>220</th><th>none</th><th>Working on it</th></tr>"
+                + "</table>"
+                + "<button type=\"submit\"  name=\"command\" value=\"Shoppingcart\" >Add to Shoppingcart</button>"
+                + "</form>";
         return cartTable;
+
 //                 <table class=table table-striped><thead><tr><th>Name</th><th>Quantity</th><th>Price</th>
 //                    <th>Total</th></tr></thead><tbody><tr>
 //                    <td>  <%=finish%></td><td> <%=quant%></td><td> <%=price%></td><td class=lineprice><%= total%></td><td><form action=Control method=POST>
@@ -33,19 +36,15 @@ public class HtmlConverter {
 //        </table>
     }
 
-  public static String carportAnlgeRooftoHtml(Carport carport) {
-            return null;
-//        String cartTable = "<table id=\"orders\">"
-//                + "<tr><th>Order ID </th><th>Length </th><th>Width </th><th>Height </th><th>User </th><th>Shipped\t</th></tr>";
-//        for (int i = 0; i < orders.size(); i++) {
-//            cartTable += "<tr><td>" + orders.get(i).getOrder_id() + " </td>"
-//                    + "<td>" + orders.get(i).getLength() + " </td>"
-//                    + "<td>" + orders.get(i).getWidth() + " </td>"
-//                    + "<td>" + orders.get(i).getHeight() + " </td>"
-//                    + "<td>" + orders.get(i).getUser().toString() + " </td>"
-//                    + "<td>" + orders.get(i).isShipped() + " </td></tr>";
-//        }
-//        return cartTable;
+    public static String carportAnlgeRooftoHtml(Carport carport) {
+        String cartTable = "<form action=\"FrontController\" method=\"GET\">"
+                + "<table id=\"carport\">"
+                + "<tr><th>Carport  </th><th>Length   </th><th>Width    </th><th>Height    </th><th>Angle   </th><th>Price      </th></tr>"
+                + "<tr><th>Size</th><th>" + carport.getLength() + "</th><th>" + carport.getWidth() + "</th><th>220</th><th>" +carport.getRoofangle()+"</th><th>Working on it</th></tr>"
+                + "</table>"
+                + "<button type=\"submit\"  name=\"command\" value=\"Shoppingcart\" >Add to Shoppingcart</button>"
+                + "</form>";
+        return cartTable;
     }
 
 }
---
+++ FogExame/src/main/java/Presentation/ShoppingCartCommand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import DataLayer.Carport;
+import Logic.CarportException;
+import Logic.CarportFacade;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author emils
+ */
+public class ShoppingCartCommand extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws CarportException {
+return "ShoppingCart";
+
+    }
+
+}
