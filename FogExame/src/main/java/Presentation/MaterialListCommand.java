f42a4629659fd2f639a3e0b48bfba5e7998ffb6a
Fred <44893926+FrederikLL@users.noreply.github.com>
2019-05-16 12:43:35 +0200

    leggo

--- FogExame/src/main/java/Presentation/MaterialListCommand.java
+++ FogExame/src/main/java/Presentation/MaterialListCommand.java
@@ -16,10 +16,14 @@ import static Logic.CalcPartList.totalpartlist;
 import Logic.CalcPrice;
 import Logic.CarportException;
 import Logic.CreateCarport;
+import Logic.Facade.CarportFacade;
 import Logic.Facade.OrderFacade;
 import static Presentation.HtmlConverter.printMetalPartList;
 import static Presentation.HtmlConverter.printPartList;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -32,34 +36,34 @@ public class MaterialListCommand extends Command {
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws CarportException {
-        int length = Integer.parseInt(request.getParameter("length"));
-        int width = Integer.parseInt(request.getParameter("width"));
-        int lengthShed = Integer.parseInt(request.getParameter("lengthShed"));
-        int widthShed = Integer.parseInt(request.getParameter("widthShed"));
-        int angle = Integer.parseInt(request.getParameter("angle"));
-
-        /*
-        ArrayList<Order> shoppingcart = new ArrayList();
-        User user = (User) request.getSession().getAttribute("user");
-         */
-        int price = CreateCarport.NumbersFlatRoof(width, length, width, length);
-        Carport cp = CreateCarport.createCarportFlatRoof(length, width, lengthShed, widthShed, price);
-        Shed sh = new Shed(lengthShed, 220, widthShed);
-        Carport carp = new Carport(length, 220, width, sh, angle, 0);
-        ArrayList<MaterialList> list = totalpartlist(carp);
-        String slist = printPartList(list);
-        
-        CalcPrice lizz = new CalcPrice();
-        ArrayList<MetalParts> mlist = lizz.metalParts(list);
-        String smlist = printMetalPartList(mlist);
-        /*
-        Order orders = OrderFacade.createOrder(1, cp, user);
-        shoppingcart.add(orders);
-         */
-        request.setAttribute("mlist", slist);
-        request.setAttribute("smlist", smlist);
-
-        return "MaterialList";
+            int length = Integer.parseInt(request.getParameter("length"));
+            int width = Integer.parseInt(request.getParameter("width"));
+            int lengthShed = Integer.parseInt(request.getParameter("lengthShed"));
+            int widthShed = Integer.parseInt(request.getParameter("widthShed"));
+            int angle = Integer.parseInt(request.getParameter("angle"));
+            
+            /*
+            ArrayList<Order> shoppingcart = new ArrayList();
+            User user = (User) request.getSession().getAttribute("user");
+            */
+            int price = CreateCarport.NumbersFlatRoof(width, length, width, length);
+            Carport cp = CreateCarport.createCarportFlatRoof(length, width, lengthShed, widthShed, price);
+            Shed sh = new Shed(lengthShed, 220, widthShed);
+            //Carport carp = new Carport(CarportFacade.getCarportLength(length), 220, CarportFacade.getCarportWidth(width), sh, angle, 0);
+            ArrayList<MaterialList> list = totalpartlist(cp);
+            String slist = printPartList(list);
+            
+            CalcPrice lizz = new CalcPrice();
+            ArrayList<MetalParts> mlist = lizz.metalParts(list);
+            String smlist = printMetalPartList(mlist);
+            /*
+            Order orders = OrderFacade.createOrder(1, cp, user);
+            shoppingcart.add(orders);
+            */
+            request.setAttribute("mlist", slist);
+            request.setAttribute("smlist", smlist);
+            
+            return "MaterialList";
     }
 
 }
